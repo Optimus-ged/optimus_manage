3acba94060bb72bdca7f13a40522fa3f68bd9311
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-01-08 19:34:57 +0200

    creation de la table fiche de stock pour une meilleure precision

--- script_db.sql
+++ script_db.sql
@@ -80,12 +80,28 @@ CREATE TABLE stock(
 ALTER TABLE stock
 ADD CONSTRAINT st_prod FOREIGN KEY (idProduit) REFERENCES produit(id)
 
+CREATE TABLE fiche_de_stock(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date_fiche_de_stock VARCHAR(30),
+    idProduit INT,
+    stock_initial FLOAT,
+    qte_entree FLOAT,
+    qte_consommee FLOAT,
+    stock_final FLOAT
+);
+
+ALTER TABLE fiche_de_stock
+ADD CONSTRAINT fk_fiche_stock FOREIGN KEY(idProduit)REFERENCES produit(id)
+
 
 
 TRUNCATE TABLE entete_facture;
 TRUNCATE TABLE detail_facture;
 TRUNCATE TABLE approentete;
-TRUNCATE TABLE detailsappro
+TRUNCATE TABLE detailsappro;
+TRUNCATE TABLE fiche_de_stock;
+TRUNCATE TABLE stock;
+TRUNCATE TABLE produit;
 
 
 SELECT  entete_facture.id,designation, detail_facture.qte, produit.pu FROM `detail_facture` INNER JOIN produit ON produit.id=detail_facture.idProduit INNER JOIN entete_facture ON entete_facture.id=detail_facture.idEnteteFacture where entete_facture.id =2
@@ -117,10 +133,32 @@ SELECT idProduit, SUM(qte) AS total_entre FROM detailsappro GROUP BY idProduit
 CREATE VIEW sum_fact_by_produit AS 
 SELECT idProduit, SUM(qte) AS total_consomme FROM detail_facture GROUP BY idProduit
 
+CREATE VIEW stock_by_date AS 
+SELECT dateStock, SUM(qte) AS total_consomme FROM detail_facture GROUP BY idProduit
+
+
 -- VIEW PRINCIPALE POUR APPROS
 CREATE VIEW view_stock AS SELECT
-st.id, designation, pu, qte,  total_entre, total_consomme, qte+total_entre-total_consomme AS gestion_stock FROM stock AS st
+st.id AS id_stock, dateStock as date_mvt, prod.id AS idProduit, designation, pu, qte AS stock_initial,  (CASE  WHEN total_entre IS NULL THEN 0 ELSE total_entre END) total_entre, (CASE WHEN total_consomme IS NULL THEN 0 ELSE total_consomme END) total_consomme, (CASE WHEN total_consomme IS NULL THEN qte ELSE qte - total_consomme END) AS stock_final FROM stock AS st
 INNER JOIN produit AS prod ON st.idProduit = prod.id
 LEFT JOIN sum_appros_by_produit as sApro ON st.idProduit = sApro.idProduit
 LEFT join sum_fact_by_produit as sFact ON st.idProduit = sFact.idProduit
+--SELECT idProduit FROM fiche_de_stock WHERE date_fiche_de_stock = DATE_FORMAT(NOW(), "%d-%m-%Y")
+--st.id AS id_stock, dateStock as date_mvt, designation, pu, qte AS stock_initial,  (CASE  WHEN total_entre IS NULL THEN 0 ELSE total_entre END) total_entre, (CASE WHEN total_consomme IS NULL THEN 0 ELSE total_consomme END) total_consomme, COALESCE(qte-total_consomme, 0) AS stock_final FROM stock AS st
+
+ INSERT INTO fiche_de_stock(
+    	date_fiche_de_stock,
+        idProduit,
+        stock_initial,
+        qte_entree,
+        qte_consommee,
+        stock_final
+    )VALUES(
+    	DATE_FORMAT(NOW(), "%d-%m-%Y"),
+        (SELECT COUNT(id) id FROM produit),
+        0,
+        0,
+        0,
+        0
+    );
 
