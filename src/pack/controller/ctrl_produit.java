ac36ef84daeac241c708cba77626d98cb41c8800
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-01-13 22:51:12 +0200

    creation du model traitement

--- src/pack/controller/ctrl_produit.java
+++ src/pack/controller/ctrl_produit.java
@@ -5,9 +5,13 @@
  */
 package pack.controller;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +19,15 @@ import javafx.fxml.Initializable;
  * @author Optimus
  */
 public class ctrl_produit implements Initializable {
+
+    @FXML
+    private Label txtId;
+    @FXML
+    private TextField txtDesignation;
+    @FXML
+    private TextField txtPu;
+    @FXML
+    private JFXButton btnEnre;
 
     /**
      * Initializes the controller class.
