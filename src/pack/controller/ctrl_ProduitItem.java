10f090ee2438d0732f3e5ee831de72049fbb66cf
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-01-27 20:18:14 +0200

    Chargement de la table produit au niveau de l'interface

--- src/pack/controller/ctrl_ProduitItem.java
+++ src/pack/controller/ctrl_ProduitItem.java
@@ -7,7 +7,9 @@ package pack.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -16,12 +18,25 @@ import javafx.fxml.Initializable;
  */
 public class ctrl_ProduitItem implements Initializable {
 
+    @FXML
+    private Label idProduit;
+    @FXML
+    private Label designation;
+    @FXML
+    private Label PU;
+    
+    public static String idProduit_;
+    public static String designation_;
+    public static String pu_;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        idProduit.setText(idProduit_);
+        designation.setText(designation_);
+        PU.setText(pu_);
     }    
     
 }
--- src/pack/controller/ctrl_produit.java
+++ src/pack/controller/ctrl_produit.java
@@ -6,18 +6,25 @@
 package pack.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.net.URL;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import pack.main.cls_controller;
-import pack.model.MdlClient;
 import pack.model.MdlProduit;
+import static pack.controller.ctrl_ProduitItem.designation_;
+import static pack.controller.ctrl_ProduitItem.pu_;
+import static pack.controller.ctrl_ProduitItem.idProduit_;
+import pack.model.MdlConnexion;
+
 
 /**
  * FXML Controller class
@@ -35,8 +42,11 @@ public class ctrl_produit implements Initializable {
     private TextField txtPu;
     @FXML
     private Label lblInfo;
+    private ResultSet res;
     @FXML
     private FontAwesomeIconView font;
+    @FXML
+    private JFXListView<?> lstview_produit;
 
     /**
      * Initializes the controller class.
@@ -45,6 +55,11 @@ public class ctrl_produit implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         ctrl = new cls_controller();
         pro = new MdlProduit(txtDesignation, txtPu, txtId, lblInfo, font);
+        initList(
+              lstview_produit,
+              "SELECT id, designation, pu FROM produit",
+              "/pack/composants/ui_ProduitItem.fxml"
+        );
     }    
 
     @FXML
@@ -52,4 +67,17 @@ public class ctrl_produit implements Initializable {
         pro.produitIn(1);
     }
     
+    private void initList(JFXListView list, String requette, String uiFx){
+        try {
+            list.getItems().clear();
+            res = MdlConnexion.getCnx().createStatement().executeQuery(requette);
+            while(res.next()){
+                idProduit_ = res.getString("id");
+                designation_ = res.getString("designation");
+                pu_ = res.getString("pu") + " Fc";
+                list.getItems().add(FXMLLoader.load(getClass().getResource(uiFx)));
+            }
+        } catch (Exception e) {
+        }
+    }
 }
