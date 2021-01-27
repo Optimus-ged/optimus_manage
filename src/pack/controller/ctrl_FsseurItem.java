3d16f101de5739aa01acf1480d587772e3e08d0a
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-01-27 20:24:09 +0200

    Ajout de l'item pour les fournisseurs

---
+++ src/pack/controller/ctrl_FsseurItem.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Optimus
+ */
+public class ctrl_FsseurItem implements Initializable {
+
+    @FXML
+    private Label idFsseur;
+    @FXML
+    private Label nom;
+    @FXML
+    private Label addresse;
+    @FXML
+    private Label contact;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
