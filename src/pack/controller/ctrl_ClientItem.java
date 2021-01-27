3a8f276978824fe7a699cce18b69c88b8d84626b
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-01-27 20:41:23 +0200

    Ajout de l'item pour les clients

---
+++ src/pack/controller/ctrl_ClientItem.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Optimus
+ */
+public class ctrl_ClientItem implements Initializable {
+
+    @FXML
+    private Label idClient;
+    @FXML
+    private Label client;
+    @FXML
+    private Label sexe;
+    @FXML
+    private Label contact;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
