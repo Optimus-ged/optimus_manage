50ec3a210812ac8fb32f814e76ae7e6a7c4f0b7a
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-01-28 14:58:22 +0200

    Correction d'erreur de saisi sur interface fournisseur

--- src/pack/model/MdlFsseur.java
+++ src/pack/model/MdlFsseur.java
@@ -18,13 +18,14 @@ import pack.traitement.TttProduit;
  * @author Optimus
  */
 public class MdlFsseur {
+
     private cls_controller ctrl;
     private TttFsseur fss;
     private TextField txtNom, txtAddresse, txtPhone;
     private Label txtId, lblInfo;
     private FontAwesomeIconView font;
-    
-    public MdlFsseur(Label txtId, Label lblInfo, TextField txtNom, TextField txtAddresse, TextField txtPhone, FontAwesomeIconView font){
+
+    public MdlFsseur(Label txtId, Label lblInfo, TextField txtNom, TextField txtAddresse, TextField txtPhone, FontAwesomeIconView font) {
         this.txtId = txtId;
         this.txtNom = txtNom;
         this.txtAddresse = txtAddresse;
@@ -35,21 +36,24 @@ public class MdlFsseur {
         ctrl = new cls_controller();
         fss = new TttFsseur();
     }
-    
-     public void fsseurIn(int btn) throws ClassNotFoundException, SQLException{
-        if(btn == 1){
-            if(champs.champs_vide.isFieldsempty(txtNom, txtAddresse, txtPhone)){
-                ctrl.showMssge(lblInfo, font, "Veuillez remplir tous les champs svp !", 0);        
-            }else{
-                fss = new TttFsseur(
-                        Integer.parseInt(txtId.getText()),
-                        txtNom.getText(), 
-                        txtAddresse.getText(), 
-                        txtPhone.getText()
-                        
-                );
-                fss.fournisseur(1);
-                ctrl.alerteInformation("Information", "Enregistre !!!");            
+
+    public void fsseurIn(int btn) throws ClassNotFoundException, SQLException {
+        if (btn == 1) {
+            if (champs.champs_vide.isFieldsempty(txtNom, txtAddresse, txtPhone)) {
+                ctrl.showMssge(lblInfo, font, "Veuillez remplir tous les champs svp !", 0);
+            } else {
+                if (ctrl.isValideTel(txtPhone.getText())) {
+                    fss = new TttFsseur(
+                            Integer.parseInt(txtId.getText()),
+                            txtNom.getText(),
+                            txtAddresse.getText(),
+                            txtPhone.getText()
+                    );
+                    fss.fournisseur(1);
+                    ctrl.alerteInformation("Information", "Enregistre !!!");
+                } else {
+                    ctrl.alerteInformation("Information", "Numero de telephone invalide !!!");
+                }
             }
         }
     }
